22bae8fb3b4f62d31da88b4b958d4c4d34366185
dub6ix <90335716+dub6ix@users.noreply.github.com>
2022-01-29 12:35:20 -0600

    appears to be working

--- mappings/constants.ts
+++ mappings/constants.ts
@@ -2,4 +2,3 @@ import { log, BigInt, store, Bytes } from '@graphprotocol/graph-ts';
 import { bigi } from './utils';
 
 export const LIQUDATION_PERIOD = bigi(1000);
-export const INTERVAL = bigi(256);
